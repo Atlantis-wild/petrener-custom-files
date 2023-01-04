23f8fb9ab2ea881492f1d55a27c0df536cf08292
BigM20006 <m1.Yaquobi@gmail.com>
2023-01-05 00:05:54 +0500

    testing object

--- custom-javascript.js
+++ custom-javascript.js
@@ -1,21 +1,4 @@
-var featuredPost1 = new Post("Aquarium Water Conditioner", "https://petrener.com/best-aquarium-water-conditioner/",
-    "https://petrener.com/wp-content/uploads/2022/12/Tetra-AquaSafe-Plus-Aquarium-Water-Conditioner_low.png");
+const featurePost1 = {postName:"Best Aquarium Water Condtioner", postLink:"https://petrener.com/best-aquarium-water-conditioner/", 
+    postImg:"https://petrener.com/wp-content/uploads/2022/12/Tetra-AquaSafe-Plus-Aquarium-Water-Conditioner_low.png"};
 
-alert(featuredPost1.postName);
-
-class Post{
-    constructor(postName, postLink, postImg){
-        this.postName = postName;
-        this.postLink = postLink;
-        this.postImg = postImg;
-    }
-    get postName(){
-        return this.postName;
-    }
-    get postLink(){
-        return this.postLink;
-    }
-    get postImg(){
-        return this.postImg;
-    }
-}
+    
